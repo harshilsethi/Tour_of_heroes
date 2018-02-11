dec00807fe67142f666a9b2d13782cdffb38ecef
Harshil Sethi <harshil@hotmail.fr>
2018-02-11 01:28:04 +0100

    Add hero & message service & message component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,26 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessageComponent } from './message/message.component';
+import {HeroService} from './hero.service';
+import {MessageService} from './message.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroService,
+    MessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
